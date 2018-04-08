b25064bb32998e7ded07a515bf0643130a32b528
Scott Janke <sjanke@gmail.com>
2018-04-08 13:35:43 -0700

    2015/24 - get solution to part 2

--- 2015/24/rust/src/main.rs
+++ 2015/24/rust/src/main.rs
@@ -15,21 +15,52 @@ fn main() {
     let total = weights.iter().sum::<i16>();
     println!("total = {}", total); // total = 1548; (total / 3) = 516; (total / 4) = 387
 
-    part_1(&weights);
+    println!("part 1:");
+    find_best_qe(&weights, 6, 516);
+
+    println!("part 2:");
+    find_best_qe(&weights, 5, 387);
 }
 
+fn find_best_qe(weights: &[i16], weight_count: usize, necessary_weight_sum: i16) {
+    let mut best_qe = i64::max_value();
+
+    choose(weight_count, weights.len(), |indexes| {
+        let (sum, product) = calculate_statistics(weights, indexes);
+        if sum == necessary_weight_sum && product <= best_qe {
+            best_qe = product;
+            print_choice(weights, indexes);
+            println!("qe = {}", best_qe);
+        }
+    });    
+}
+
+fn print_choice(weights: &[i16], indexes: &[usize]) {
+    for weight_index in indexes {
+        print!("{} ", weights[*weight_index]);
+    }
+}
+
+/*
+fn part_2(weights: &[i16]) {
+    let mut best_qe = i64::max_value();
+
+    choose(5, weights.len(), |indexes| {
+        let (sum, product) = calculate_statistics(weights, indexes);
+        if sum == 387 && product <= best_qe {
+            best_qe = product;
+            print_choice(weights, indexes);
+            println!("qe = {}", best_qe);
+        }
+    });
+}
+*/
+/*
 fn part_1(weights: &[i16]) {
-    let mut best_weights = (0..6).map(|_| -1).collect::<Vec<i16>>();
     let mut best_qe = i64::max_value();
 
     choose(6, weights.len(), |indexes| {
-        let mut sum = 0i16;
-        let mut product = 1i64;
-        for i in indexes {
-            let w = weights[*i];
-            sum += w;
-            product *= w as i64;
-        }
+        
         if sum == 516 && product <= best_qe {
             best_qe = product;
             for (loop_index, weight_index) in indexes.iter().enumerate() {
@@ -41,6 +72,18 @@ fn part_1(weights: &[i16]) {
             println!("qe = {}", best_qe);
         }
     });
+}
+*/
+
+fn calculate_statistics(weights: &[i16], indexes: &[usize]) -> (i16, i64) {
+    let mut sum = 0i16;
+    let mut product = 1i64;
+    for i in indexes {
+        let w = weights[*i];
+        sum += w;
+        product *= w as i64;
+    }
+    (sum, product)
 }
 
 fn choose<F: FnMut(&[usize])>(choose_count: usize, total_item_count: usize, mut indexes_callback: F) {
