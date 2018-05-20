a4a6a9b176de8bd7f91f91a8b80b815b1373577a
Scott Janke <sjanke@gmail.com>
2018-05-19 22:52:19 -0700

    2017/5 part 2

--- 2017/5/src/main.rs
+++ 2017/5/src/main.rs
@@ -1,8 +1,9 @@
+use std::borrow::BorrowMut;
 use std::fs;
 use std::str::FromStr;
 
 fn main() {
-    let mut instructions =
+    let instructions =
     fs::read_to_string("C:\\Users\\jankes\\Documents\\AdventOfCode\\2017\\5\\instructions.txt")
     .expect("couldn't read instructions file")
     .lines()
@@ -14,13 +15,35 @@ fn main() {
         return;
     }
 
-    let mut count = 0;
+    let count = run_part_1(instructions.clone().borrow_mut());
+    println!("part 1: program finishes after {} instructions", count);
+
+    let count = run_part_2(instructions.clone().borrow_mut());
+    println!("part 2: program finishes after {} instructions", count);
+}
+
+fn run_part_1(instructions: &mut [i16]) -> u32 {
+    run(instructions, |previous, instructions| instructions[previous as usize] += 1)
+}
+
+fn run_part_2(instructions: &mut [i16]) -> u32 {
+    run(instructions, |previous, instructions| {
+        if instructions[previous as usize] >= 3 {
+            instructions[previous as usize] -= 1;
+        } else {
+            instructions[previous as usize] += 1;
+        }        
+    })
+}
+
+fn run<F: FnMut(i16, &mut [i16])>(instructions: &mut [i16], mut update: F) -> u32 {
+    let mut count = 0u32;
     let mut current = 0i16;
     while 0 <= current && current < instructions.len() as i16 {
         let previous = current;
         current += instructions[current as usize];
         count += 1;
-        instructions[previous as usize] += 1;
+        update(previous, instructions);
     }
-    println!("program finishes after {} instructions", count);
+    return count;
 }
