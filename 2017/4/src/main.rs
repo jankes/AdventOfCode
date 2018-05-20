57ab3092721489ff7fa4b7f4aacb4b854ac83f93
Scott Janke <sjanke@gmail.com>
2018-05-19 21:46:42 -0700

    2017/4 part 2

--- 2017/4/src/main.rs
+++ 2017/4/src/main.rs
@@ -1,19 +1,21 @@
-use std::borrow::Borrow;
 use std::collections::HashSet;
 use std::fs;
 
 fn main() {    
-    let valid_passphrase_count = count_valid_passphrases(
-        fs::read_to_string("C:\\Users\\jankes\\Documents\\AdventOfCode\\2017\\4\\passphrases.txt")
-            .expect("couldn't read passphrases file to a string")
-            .borrow()
-    );
-    println!("There are {} valid passphrases", valid_passphrase_count);
+    let all_passphrases = fs::read_to_string("C:\\Users\\jankes\\Documents\\AdventOfCode\\2017\\4\\passphrases.txt")
+                              .expect("couldn't read passphrases file to a string");
+
+    let valid_passphrase_count = count_valid_passphrases_part_1(&all_passphrases);
+    println!("part 1: there are {} valid passphrases", valid_passphrase_count);
+
+    let valid_passphrase_count = count_valid_passphrases_part_2(&all_passphrases);
+    println!("part 2: there are {} valid passphrases", valid_passphrase_count);
 }
 
-fn count_valid_passphrases(all_phrases: &str) -> u16 {
+fn count_valid_passphrases_part_1(all_phrases: &str) -> u16 {
     let mut set = HashSet::<&str>::with_capacity(20);
-    all_phrases.lines()
+    all_phrases
+    .lines()
     .map(|phrase| {
         set.clear();
         for word in phrase.split_whitespace() {
@@ -24,4 +26,40 @@ fn count_valid_passphrases(all_phrases: &str) -> u16 {
         return 1u16;
     })
     .sum::<u16>()
+}
+
+fn count_valid_passphrases_part_2(all_phrases: &str) -> u16 {
+    let mut words = Vec::<&[u8]>::with_capacity(20);
+    all_phrases
+    .lines()
+    .map(|phrase| {
+        words.clear();
+        for word in phrase.split_whitespace().map(|p| p.as_bytes()) {
+            words.push(word);
+        }
+        for (i, first_word) in words.iter().enumerate() {
+            for second_word in words.iter().skip(i + 1) {
+                if are_anagrams(first_word, second_word) {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    })
+    .sum::<u16>()
+}
+
+fn are_anagrams(first_word: &[u8], second_word: &[u8]) -> bool {
+    if first_word.len() != second_word.len() {
+        return false;
+    }
+    let mut first_word_sorted = Vec::with_capacity(first_word.len());
+    first_word_sorted.extend_from_slice(first_word);
+    first_word_sorted.sort();
+
+    let mut second_word_sorted = Vec::with_capacity(second_word.len());
+    second_word_sorted.extend_from_slice(second_word);
+    second_word_sorted.sort();
+
+    return first_word_sorted == second_word_sorted
 }
