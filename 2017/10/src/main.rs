448871588bbf9ea203aeee12cb3dec9305549ec7
Scott Janke <sjanke@gmail.com>
2018-06-24 00:02:15 -0700

    2017/10 part 2 solution

--- 2017/10/src/main.rs
+++ 2017/10/src/main.rs
@@ -1,26 +1,63 @@
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
+    let lengths = [102u8, 255, 99, 252, 200, 24, 219, 57, 103, 2, 226, 254, 1, 0, 69, 216];
     let mut list = [0u8; 256];
+    knot_hash(&mut list, &lengths, 1);
+    println!("part 1: first two numbers in list: {} {}", list[0], list[1]);
+}
+
+fn part_2() {
+    let lengths = b"102,255,99,252,200,24,219,57,103,2,226,254,1,0,69,216\x11\x1F\x49\x2F\x17";
+    let mut list = [0u8; 256];
+    knot_hash(&mut list, lengths, 64);
+    let dense = [
+        xor(&list[0..16]),
+        xor(&list[16..32]),
+        xor(&list[32..48]),
+        xor(&list[48..64]),
+        xor(&list[64..80]),
+        xor(&list[80..96]),
+        xor(&list[96..112]),
+        xor(&list[112..128]),
+        xor(&list[128..144]),
+        xor(&list[144..160]),
+        xor(&list[160..176]),
+        xor(&list[176..192]),
+        xor(&list[192..208]),
+        xor(&list[208..224]),
+        xor(&list[224..240]),
+        xor(&list[240..256]),
+    ];
+    println!("part 2: {:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}",
+             dense[0], dense[1], dense[2], dense[3], dense[4], dense[5], dense[6], dense[7],
+             dense[8], dense[9], dense[10], dense[11], dense[12], dense[13], dense[14], dense[15]);
+}
+
+fn xor(list: &[u8]) -> u8 {
+    let mut iter = list.iter();
+    let first = *iter.next().unwrap();
+    iter.fold(first, |result, &next| result ^ next)
+}
+
+fn knot_hash(list: &mut [u8; 256], lengths: &[u8], rounds: u8) {
     for i in 0u8..=255 {
         list[i as usize] = i;
     }
-
-    let lengths = [102u8, 255, 99, 252, 200, 24, 219, 57, 103, 2, 226, 254, 1, 0, 69, 216];
-
     let mut current_position = 0u8;
     let mut skip_size = 0u8;
-    for length in &lengths {        
-        reverse(&mut list, current_position, *length);
-        current_position = current_position.wrapping_add(*length);
-        current_position = current_position.wrapping_add(skip_size);
-        skip_size += 1;
+    for _ in 0..rounds {
+        for length in lengths {        
+            reverse(&mut *list, current_position, *length);
+            current_position = current_position.wrapping_add(*length);
+            current_position = current_position.wrapping_add(skip_size);
+            skip_size = skip_size.wrapping_add(1);
+        }
     }
-
-    println!("first two numbers in list: {} {}", list[0], list[1]);
 }
 
 fn reverse(list: &mut [u8], mut start_index: u8, length: u8) {
@@ -32,55 +69,3 @@ fn reverse(list: &mut [u8], mut start_index: u8, length: u8) {
         end_index = end_index.wrapping_sub(1);
     }
 }
-
-// fn example() {
-//     let mut list = [0u8, 1, 2, 3, 4];
-//     let lengths = [3u8, 4, 1, 5];
-
-//     let mut current_position = 0u8;
-//     let mut skip_size = 0u8;
-
-//     println!("initial:");
-//     print_stats(&list, current_position);
-
-//     for length in &lengths {
-//         reverse_mod5(&mut list, current_position, *length);
-//         current_position = wrapping_add(current_position, *length);
-//         current_position = wrapping_add(current_position, skip_size);
-//         skip_size += 1;
-
-//         println!("{}:", skip_size);
-//         print_stats(&list, current_position);
-//     }
-// }
-
-// fn print_stats(list: &[u8], current_position: u8) {
-//     println!("current position = {}", current_position);
-//     print!("list = ");
-//     for number in list {
-//         print!("{} ", number);
-//     }
-//     println!();
-// }
-
-// fn reverse_mod5(list: &mut [u8], mut start_index: u8, length: u8) {
-//     let count = length / 2;
-//     let mut end_index = wrapping_sub(wrapping_add(start_index, length), 1);
-//     for _ in 0..count {
-//         list.swap(start_index as usize, end_index as usize);
-//         start_index = wrapping_add(start_index, 1);
-//         end_index = wrapping_sub(end_index, 1);
-//     }
-// }
-
-// fn wrapping_add(a: u8, b: u8) -> u8 {
-//     (((a as u16) + (b as u16)) % 5u16) as u8
-// }
-
-// fn wrapping_sub(a: u8, b: u8) -> u8 {
-//     if a >= b {
-//         a - b
-//     } else {
-//         5 - b + a
-//     }
-// }
